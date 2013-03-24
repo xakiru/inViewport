76dca62b8175cd8a1827ab9f96a3052b69fe0874
Xakiru <xakiru@gmail.com>
2013-03-24 00:58:03 +0000

    commit

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
-inViewport
+$(selector).inViewport()
 ==========
 
-inViewport is a simple jquery plugin to test if an element is visible on the screen, usage : $("#element").inViewport();
+# inViewport function for jQuery
+
+Lightweight function (700 octets) that add the function inViewport to javascript.
+
+## Usage
+
+Just make class `.waterfall` on container and that’s all.
+```html
+<div id="item">Item 1</div>
+```
+ It's simple to use $("#element").inViewport();  is a simple jquery plugin to test if an element is visible on the screen: 
+```html
+<script>
+$(function () {
+
+	if($('#items').inViewport()){
+		// code if the element is visible
+		alert('items is visible');
+	}
+
+});
+</script>
+```
+
+## Options
+
+Options could be either configured in the library :
+```javascript
+var defaultDirection = 'both';  //both,vertical,horizontal
+var defaultFull = false ;       // true, false
+```
+
+or we can specify the direction and the parametre for testing the full element or just part of it:
+```javascript
+$('#selector').inViewport('vertical',false)
+```
---
+++ inviewport.js
@@ -0,0 +1,42 @@
+﻿/**
+* A jquery inViewport plugin. supports full/partial and vertical/horizontal/both detection.
+* @created 2013-3-24
+* @author Xakiru
+* @see https://github.com/Xakiru/inViewport
+* @example $("#element").inViewport('vertical',false);
+*/
+
+
+$.fn.inViewport = function(dir, full){
+//  config
+var defaultDirection = 'both';  //both,vertical,horizontal
+var defaultFull = false ;       // true, false
+
+//  
+dir = (dir || defaultDirection); 
+full = (full || defaultFull);
+
+var v = (dir == 'vertical' || dir == 'both') ? true : false;
+var h = (dir == 'horizontal' || dir == 'both') ? true : false;
+
+var win = $(window);
+var wtop = win.scrollTop() ;
+var wleft = win.scrollLeft() ;
+var wbuttom = win.scrollTop() + win.height() ;
+var wright = win.scrollLeft() + win.width() ;
+if(full){
+    var etop = this.offset().top;
+    var eleft = this.offset().left;
+    var ebuttom = this.offset().top +  this.outerHeight();
+    var eright = this.offset().left + this.outerWidth();
+}
+else{
+    var etop =  this.offset().top +  this.outerHeight();
+    var eleft =  this.offset().left + this.outerWidth();
+    var ebuttom = this.offset().top;
+    var eright =  this.offset().left;
+}
+var in_h = ( (((eright<wright) && (eleft>wleft))    )  );
+var in_v = ( (((etop>wtop)     && (ebuttom<wbuttom)) ) );
+return (((v && in_v) || (h && in_h)) && !(v && h))||(((v && in_v) && (h && in_h) )&& (v && h))
+};
